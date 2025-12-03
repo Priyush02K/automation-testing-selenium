c72c7ff0c63f6df633a795f9cf57a918578e5fae
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2025-12-03 22:35:35 +0530

    DataTypeDemo

---
+++ Day02/DataTypeDemo.java
@@ -0,0 +1,45 @@
+package Day02;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+
+		//Numeric datatype
+		int a=100 , b=200;
+		System.out.println("the value of a is :" +a );
+		System.out.println("the value of b is :" +b );
+		System.out.println("the of a and b is : " +(a+b) );
+		
+		byte by=125;
+		System.out.println(by);
+		
+		short sh =3535;
+		System.out.println(sh);
+	
+		long l=1254854558L;    //we required literal
+		System.out.println(l);
+
+		//Decimal
+		
+		float item_price=15.5F;
+		System.out.println(item_price);
+		
+		double db=123.4578;
+		System.out.println(db);
+		
+		char grade= 'A';
+		System.out.println(grade);
+		
+		
+		String name="Priyush";
+		System.out.println(name);
+
+
+
+		boolean bl=true;
+		System.out.println(bl);
+
+
+	}
+
+}
---
+++ Day02/VariableDemo.java
@@ -0,0 +1,36 @@
+package Day02;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int a;  // declrartion  		
+		//a=125;  //assignment
+		
+		
+		// int a=125; //declrartion + assignment
+		// System.out.println(a);
+		
+		//approach 01
+		/* int a=100;
+		 int b=200;
+		 int c=300;
+		 */
+		//approach 02
+	
+		/*int a , b ,b ; //  if only same data type only
+		a=100;
+		b=200;
+		c=300;
+		*/
+		
+		
+		//approach 03
+		int a=100, b=200, c=300;
+		System.out.println("the value of a is :" +a );
+		System.out.println("the value of b is :" +b );
+		System.out.println("the value of b is :" +c );
+
+	}
+
+}
